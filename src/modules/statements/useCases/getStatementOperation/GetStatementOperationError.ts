f7de24e17a02d2278d36c78dcd693f18c14df544
Victor Yuri <victoryuri.af@gmail.com>
2022-11-28 07:56:27 -0300

    rocketseat-nodejs-challenges-07-testes-unitarios

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationError.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationError.ts
@@ -1,15 +1,15 @@
 import { AppError } from "../../../../shared/errors/AppError";
 
-export namespace CreateStatementError {
+export namespace GetStatementOperationError {
   export class UserNotFound extends AppError {
     constructor() {
       super('User not found', 404);
     }
   }
 
-  export class InsufficientFunds extends AppError {
+  export class StatementNotFound extends AppError {
     constructor() {
-      super('Insufficient funds', 400);
+      super('Statement not found', 404);
     }
   }
 }
